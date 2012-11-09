d218adf29253a7b653ac4af00407e28bdf754e20
Zadrutsky Sergey <sergey.zadrutsky@gmail.com>
2012-11-09 15:06:03 +1100

    InitialCommit
    Initial

---
+++ Controllers/KeyboardController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Module2.Controllers
+{
+    public class KeyboardController : Controller
+    {
+        //
+        // GET: /Keyboard/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Models/KeyboardModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Module2.Models
+{
+    public class KeyboardModel
+    {
+    }
+}
